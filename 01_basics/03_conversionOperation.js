3ff4afe8cf1827fc040afcc6d44f78b9cbe8074b
waqar-akram <80595182+waqar-akram@users.noreply.github.com>
2025-02-24 07:23:48 +0000

    conversion Operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+let score = 33
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let newscore = String(score);
+//console.log(newscore);
+
+let isLoggedIn = ""
+// let boolenIsLoggedIn = Boolean(isLoggedIn);
+// console.log(boolenIsLoggedIn)
+
+// 1 is convert into => True and 0 convert into => False
+// "" blank string give you False 
+// "Vicky" this string give you True
+
+// ########## Operation #####################
+
+// let age = 33
+// let negAge = -age
+// console.log(age);
+// console.log(negAge)
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
+
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,8 @@
+console.log ("2" > 1)
+console.log (""> 1)
+
+// conversion is not possible in that case 
+console.log("2" === 2)
+
+// conversion is possible in that case
+console.log("2" == 2 )
