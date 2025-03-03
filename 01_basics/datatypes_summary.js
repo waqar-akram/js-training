a04f3b76e41721e62c266f6c18c7103ab3f2783b
waqar-akram <80595182+waqar-akram@users.noreply.github.com>
2025-03-03 06:19:13 +0000

    datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,21 @@
+// Primitive Data Types 
+// (there are almost 7 types of Primitive data types : String, Number, Boolean, Null, Undefined, Symbol, BigInt).
+
+//Non Primitive Data Types also know as Reference Type
+// (Array, Objects, Functions).
+
+// Arry we define like this 
+const testing = ["Waqar", "Shahab", "Akram"];
+
+// Object define like this.
+const myObj = {
+    name: "Waqar",
+    age: "28",
+    city: "Karachi",
+}
+console.log(myObj)
+// Now we define about Functions.
+const myFunction = function(){
+    console.log("Hello World...")
+}
+console.log(myFunction())
