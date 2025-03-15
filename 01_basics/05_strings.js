04418c6604dcba9e75148080e181eddfc36120b7
waqar-akram <80595182+waqar-akram@users.noreply.github.com>
2025-03-15 07:33:52 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,15 @@
+// `the above give sign name is backticks`
+const name = "Vicky"
+const value = 50
+const myname = "my-name-is-waqar-akram."
+// string always use in backticks sign like this ${}
+
+console.log(`My Name is ${name} and my Score is ${value}`)
+
+console.log(name[0])
+console.log(name.__proto__)
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.includes("Vicky"))
+console.log(myname.split("-"))
